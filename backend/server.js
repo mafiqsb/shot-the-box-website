446d131b611fe1fc056ebd33f549638ad2d32d0d
mafiqsb <afiqsam71@gmail.com>
2024-02-27 13:38:37 +0800

    updated 3 : 27.2.2024

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 const imageRouter = require('./Routes/imageRoutes.js');
 const seedRouter = require('./Routes/seedRoutes.js');
 const uploadRouter = require('./Routes/uploadRoutes.js');
@@ -8,9 +8,24 @@ const userRouter = require('./Routes/userRoutes.js');
 const dotenv = require('dotenv');
 const emailnotifyRouter = require('./Routes/emailnotifyRoutes.js');
 
-const app = express();
-
 dotenv.config();
+
+const app = express();
+const __dirname = path.resolve();
+
+// Serve frontend on /frontend route
+
+app.use('/frontend', express.static(path.join(__dirname, 'frontend', 'build')));
+app.get('/frontend/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
+
+// Serve frontend on /admin route
+
+app.use('/admin', express.static(path.join(__dirname, 'admin', 'build')));
+app.get('/admin/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'admin', 'build', 'index.html'));
+});
 
 app.use(cors());
 app.use(express.json());
