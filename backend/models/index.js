0740eebe6fb497e509187b60dbeb050b1b242eff
mafiqsb <afiqsam71@gmail.com>
2024-02-28 11:36:32 +0800

    Updated 1: Edit at the env

--- backend/models/index.js
+++ backend/models/index.js
@@ -10,15 +10,36 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+//   );
+// }
+
+if (process.env.DATABASE_URL) {
+  // Use the external database URL provided by Render
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    host: 'dpg-cnfaf0icn0vc73e6os00-a',
+    dialect: 'postgres',
+    ssl: {
+      rejectUnauthorized: false, // Set to false if using a self-signed certificate
+    },
+  });
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  // Fallback to individual connection parameters
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    host: 'your-database-host.render.com', // Replace with your Render PostgreSQL host
+    dialect: 'postgres',
+    ssl: {
+      rejectUnauthorized: false, // Set to false if using a self-signed certificate
+    },
+  });
 }
 
 fs.readdirSync(__dirname)
