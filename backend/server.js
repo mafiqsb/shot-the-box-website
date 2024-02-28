b71a2127d2b90215324a62b8a5e0cfaede8545e4
mafiqsb <afiqsam71@gmail.com>
2024-02-28 18:58:31 +0800

    updated 15

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 // });
 
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-app.get('*', (req, res) => {
+app.get('../frontend/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
@@ -33,7 +33,7 @@ app.get('*', (req, res) => {
 // });
 
 app.use(express.static(path.join(__dirname, '/admin/build')));
-app.get('*', (req, res) => {
+app.get('../admin/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'admin/build/index.html'));
 });
 
