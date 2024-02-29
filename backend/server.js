2101ce2c40575b7bfccb4decfa1ee160e9a8522c
mafiqsb <afiqsam71@gmail.com>
2024-02-29 10:48:22 +0800

    updated 10

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const userRouter = require('./Routes/userRoutes.js');
 const dotenv = require('dotenv');
 const emailnotifyRouter = require('./Routes/emailnotifyRoutes.js');
 
+const { Sequelize } = require('sequelize');
+
 dotenv.config();
 
 const app = express();
@@ -24,6 +26,31 @@ app.get('/admin/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'admin', 'build', 'index.html'));
 });
 
+app.get('/api/test-database', async (req, res) => {
+  try {
+    // Replace these with your actual database connection details
+    const sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // Set to false if using a self-signed certificate
+        },
+      },
+    });
+
+    // Test the database connection
+    await sequelize.authenticate();
+
+    // If the connection is successful, send a success response
+    res.status(200).json({ message: 'Database connection test successful' });
+  } catch (error) {
+    // If there's an error, send an error response
+    console.error('Error connecting to the database:', error.message);
+    res.status(500).json({ error: 'Database connection test failed' });
+  }
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -36,10 +63,6 @@ app.use('/api/emailnotify', emailnotifyRouter);
 
 const port = process.env.PORT || 8000;
 const host = process.env.HOST || '0.0.0.0';
-
-// app.listen(port, () => {
-//   console.log(`server at http://localhost:${port}`);
-// });
 
 app.listen(port, host, () => {
   console.log(`server at http://${host}:${port}`);
