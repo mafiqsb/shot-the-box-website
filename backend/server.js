6fac947364db532e41ff480ba9d10f11b7225ac5
mafiqsb <afiqsam71@gmail.com>
2024-02-29 09:45:40 +0800

    updated 9

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,12 @@ app.use('/api/user', userRouter);
 app.use('/api/emailnotify', emailnotifyRouter);
 
 const port = process.env.PORT || 8000;
+const host = process.env.HOST || '0.0.0.0';
 
-app.listen(port, () => {
-  console.log(`server at http://localhost:${port}`);
+// app.listen(port, () => {
+//   console.log(`server at http://localhost:${port}`);
+// });
+
+app.listen(port, host, () => {
+  console.log(`server at http://${host}:${port}`);
 });
