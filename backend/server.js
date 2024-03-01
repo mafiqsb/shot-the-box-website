55314ac4e3471b651e2aabcf1970dbb4be6a7225
mafiqsb <afiqsam71@gmail.com>
2024-03-01 11:11:32 +0800

    updated 18

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ try {
 // Serve frontend on /frontend route
 
 app.use('/frontend', express.static(path.join(__dirname, './build')));
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   try {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
     console.log('successful join');
@@ -78,7 +78,7 @@ app.get('/', (req, res) => {
 
 // Serve admin on /admin route
 app.use('/admin', express.static(path.join(__dirname, './build')));
-app.get('/admin', (req, res) => {
+app.get('*', (req, res) => {
   try {
     res.sendFile(path.join(__dirname, '../admin/build', 'index.html'));
     console.log('successful join');
