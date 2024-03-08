af4d16a55be9b13e520bed570f5373bfc6783a53
mafiqsb <afiqsam71@gmail.com>
2024-03-08 16:07:19 +0800

    updated 17

--- backend/migrations/20240227101809-create-imagedetails.js
+++ backend/migrations/20240227101809-create-imagedetails.js
@@ -26,7 +26,7 @@ module.exports = {
       //   allowNull: true,
       // },
       confirmimages: {
-        type: Sequelize.ARRAY(Sequelize.TEXT),
+        type: Sequelize.ARRAY,
         allowNull: true,
       },
       // confirmimages: {
