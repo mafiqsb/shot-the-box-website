b923145f13bbce482214282391a5e4ca6a2fac89
mafiqsb <afiqsam71@gmail.com>
2024-03-11 12:50:58 +0800

    updated 3

--- backend/migrations/20240311023654-updated_imagedetails_confirmimages_1.js
+++ backend/migrations/20240311023654-updated_imagedetails_confirmimages_1.js
@@ -5,7 +5,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return Promise.all([
       queryInterface.changeColumn('imagedetails', 'confirmimages', {
-        type: Sequelize.STRING(5000),
+        type: Sequelize.ARRAY(Sequelize.STRING),
         allowNull: true,
       }),
     ]);
